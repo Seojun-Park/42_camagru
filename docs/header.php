72f64787af24cf3041c433eabefc8f64c0c67c31
Jinchul park <jinchul112@gmail.com>
2020-09-26 16:40:56 +0200

    u

--- docs/header.php
+++ docs/header.php
@@ -1,3 +1,7 @@
+<?php
+include 'inc_head.php';
+?>
+
 <header>
     <div class="header_row first_col">
         <a href="#"><img alt="logo" id="logo" src="../asset/logo.png" /></a>
@@ -6,11 +10,24 @@
         <input type="text" id="searchbar" placeholder="Search user" />
     </div>
     <div class="header_row third_col">
-        auth
+        <?php
+        if ($login == FALSE) {
+            ?>
+            <a href="login.html">
+                LOGIN</a>
+        <?php
+        } else {
+            ?>
+            <!-- <a href="#">LOGOUT</a> -->
+            <form method="post">
+                <input type="submit" name="logout" id="logout" value="LOGOUT" />
+            </form>
+        <?php
+            if (array_key_exists('logout', $_POST)) {
+                session_destroy();
+                echo "<meta http-equiv='refresh' content='0;url=index.php'>";
+            }
+        }
+        ?>
     </div>
 </header>
-<div class="wrapper">
-    <div class="page_frame">
-        lala
-    </div>
-</div>
--- docs/main.php
+++ docs/main.php
@@ -16,36 +16,9 @@ include 'func_view.php';
 </head>
 
 <body>
-
-    <header>
-        <div class="header_row first_col">
-            <a href="#"><img alt="logo" id="logo" src="../asset/logo.png" /></a>
-        </div>
-        <div class="header_row second_col">
-            <input type="text" id="searchbar" placeholder="Search user" />
-        </div>
-        <div class="header_row third_col">
-            <?php
-            if ($login == FALSE) {
-                ?>
-                <a href="login.html">
-                    LOGIN</a>
-            <?php
-            } else {
-                ?>
-                <!-- <a href="#">LOGOUT</a> -->
-                <form method="post">
-                    <input type="submit" name="logout" id="logout" value="LOGOUT" />
-                </form>
-            <?php
-                if (array_key_exists('logout', $_POST)) {
-                    session_destroy();
-                    echo "<meta http-equiv='refresh' content='0;url=index.php'>";
-                }
-            }
-            ?>
-        </div>
-    </header>
+    <?php
+    echo view('header.php');
+    ?>
     <div class="wrapper">
         <div class="page_frame">
             lala
