fdc477346b183aecda87a9abceca126171b66229
Jinchul park <jinchul112@gmail.com>
2020-09-26 11:49:07 +0200

    u

--- docs/index.php
+++ docs/index.php
@@ -9,10 +9,23 @@ include 'inc_head.php';
     <meta charset="utf-8" />
     <title>Camagru</title>
     <link rel="stylesheet" href="styles/reset.css" />
+    <link rel="stylesheet" href="styles/header.css" />
+    <link rel="stylesheet" href="styles/frame.css" />
+
 </head>
 
 <body>
-    lalala
+    <!-- header -->
+    <header>
+        <div class="header_row">
+            row
+        </div>
+    </header>
+    <div class="wrapper">
+        <div class="page_frame">
+            lala
+        </div>
+    </div>
 </body>
 
 </html>
