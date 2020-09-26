17b1f5e1b176473ed4678eee4742641facc37417
Jinchul park <jinchul112@gmail.com>
2020-09-26 12:25:54 +0200

    u

--- docs/index.php
+++ docs/index.php
@@ -17,14 +17,15 @@ include 'inc_head.php';
 <body>
     <!-- header -->
     <header>
-        <div class="header_row first_row">
+        <div class="header_row first_col">
             <a href="#"><img alt="logo" id="logo" src="../asset/logo.png" /></a>
         </div>
-        <div class="header_row second_row">
-            row
+        <div class="header_row second_col">
+            <input type="text" id="searchbar" placeholder="Search user" />
         </div>
-        <div class="header_row third_row">
-            row
+        <div class="header_row third_col">
+            <a href="#"><img alt="logo" id="profile" src="../asset/profile.png" /></a>
+
         </div>
     </header>
     <div class="wrapper">
