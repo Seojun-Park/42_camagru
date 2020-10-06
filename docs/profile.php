acba0737e7dd44f0c23d21c0f6962bc5995abd4b
Jinchul park <jinchul112@gmail.com>
2020-10-06 17:40:57 +0200

    u

--- docs/profile.php
+++ docs/profile.php
@@ -18,9 +18,6 @@ $uri = $_SERVER['REQUEST_URI']; //uri를 구함
 $query = substr($uri, strpos($uri, "?") + 1, strlen($uri));
 $obj = unserialize(file_get_contents("../db/account"));
 $profile = getProfile($query, $obj);
-if ($profile == FALSE) {
-    echo "nooo";
-}
 ?>
 <!DOCTYPE html>
 <html>
