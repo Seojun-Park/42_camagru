3efbb29bdd2a7557d4d2cfb0027c39d1b4667266
Jinchul park <jinchul112@gmail.com>
2020-10-13 13:34:21 +0200

    u

--- server/sources/start.sh
+++
@@ -1,5 +0,0 @@
-mkdir var/www/localhost
-mv ./default etc/nginx/sites-available
-ln -s etc/nginx/sites-available/default etc/nginx/sites-enabled
-chown -R www-data /var/www/*
-chmod -R 755 /var/www/*
