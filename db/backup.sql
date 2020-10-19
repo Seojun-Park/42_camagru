7f552ec7fa9569836f09a4dde40cfb6fbe9394f9
Jinchul park <jinchul112@gmail.com>
2020-10-19 17:07:25 +0200

    u

--- db/backup.sql
+++ db/backup.sql
@@ -1,3 +1,12 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.5
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Generation Time: Jul 03, 2018 at 06:43 PM
+-- Server version: 5.6.20
+-- PHP Version: 5.5.15
+
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
 START TRANSACTION;
@@ -10,7 +19,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `db_cama`
+-- Database: `post_board`
 --
 CREATE DATABASE IF NOT EXISTS `db_cama` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
 USE `db_cama`;
@@ -18,16 +27,16 @@ USE `db_cama`;
 -- --------------------------------------------------------
 
 --
--- Table structure for table `feed`
+-- Table structure for table `board`
 --
 
-CREATE TABLE `feed` (
-  `idx` int NOT NULL AUTO_INCREMENT,
+CREATE TABLE `board` (
+  `idx` int(11) NOT NULL,
   `name` varchar(100) NOT NULL,
+  `title` varchar(100) NOT NULL,
   `content` text NOT NULL,
   `date` date NOT NULL,
-  `file` VARCHAR(1000),
-  PRIMARY KEY (`idx`)
+  `file` VARCHAR(1000)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
@@ -35,12 +44,11 @@ CREATE TABLE `feed` (
 --
 
 CREATE TABLE `reply` (
-  `idx` int NOT NULL AUTO_INCREMENT,
-  `feed_num` int(11) NOT NULL,
+  `idx` int(11) NOT NULL,
+  `con_num` int(11) NOT NULL,
   `name` varchar(100) NOT NULL,
   `content` text NOT NULL,
-  `date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`idx`)
+  `date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 
@@ -53,7 +61,6 @@ CREATE TABLE `member` (
     `email` VARCHAR(100) NOT NULL ,
     `avatar` VARCHAR(2000) NOT NULL ,
     `username` VARCHAR(20) NOT NULL,
-    `isfollowing` BOOLEAN DEFAULT false,
     PRIMARY KEY (`idx`)
     ) ENGINE = InnoDB DEFAULT CHARSET=utf8;
 
@@ -62,33 +69,46 @@ CREATE TABLE `member` (
 -- Dumping data for table `board`
 --
 
+INSERT INTO `board` (`idx`, `name`, `title`, `content`, `date`) VALUES
+(1, 'S Writer', '자유게시판입니다.', '자유게시판입니다.', '2016-07-02'),
+(2, 'S Writer', '자유게시판입니다.', '자유게시판입니다.', '2016-07-02'),
+(3, 'S Writer', '자유게시판입니다.', '자유게시판입니다.', '2016-07-02'),
+(4, 'S Writer', '자유게시판입니다.', '자유게시판입니다.', '2016-07-02'),
+(5, 'S Writer', '자유게시판입니다.', '자유게시판입니다.', '2016-07-02');
+
 
 --
 -- reply's dummy data
 --
 
+INSERT INTO `reply` (`idx`, `con_num`, `name`, `content`, `date`) VALUES
+(1, 7, 'admin', '1234', '2018-04-02 19:47:19'),
+(2, 7, 'admin', '댓글 테스트', '2018-04-02 19:47:26');
+
 --
 -- Indexes for table `board`
 --
--- ALTER TABLE `board`
---   ADD PRIMARY KEY (`idx`);
+ALTER TABLE `board`
+  ADD PRIMARY KEY (`idx`);
 
 --
 -- AUTO_INCREMENT for dumped tables
 --
--- ALTER TABLE `reply`
---   ADD PRIMARY KEY (`idx`);
+ALTER TABLE `reply`
+  ADD PRIMARY KEY (`idx`);
+
+--
+-- AUTO_INCREMENT for table `board`
+--
+ALTER TABLE `board`
+  MODIFY `idx` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `board`
 --
 
---
--- AUTO_INCREMENT for table `board`
---
-
--- ALTER TABLE `reply`
---   MODIFY `idx` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+ALTER TABLE `reply`
+  MODIFY `idx` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 COMMIT;
 
--- srcs/page/main.php
+++ srcs/page/main.php
@@ -1,7 +1,6 @@
 <?php
 include $_SERVER['DOCUMENT_ROOT'] . "/db.php";
 include $_SERVER['DOCUMENT_ROOT'] . "/hooks/func_view.php";
-
 ?>
 <!DOCTYPE html>
 
@@ -13,11 +12,29 @@ include $_SERVER['DOCUMENT_ROOT'] . "/hooks/func_view.php";
 
 <body>
     <?php
-    $sql = mq("select * from member where id='{$_SESSION['userid']}'");
-    $member = $sql->fetch_array();
+    // $sql = mq("select * from member where id='{$_SESSION['userid']}'");
+    // $member = $sql->fetch_array();
+    // $sql = mq("select * from board order by idx");
+    // $list = 5;
     if (isset($_SESSION['userid'])) { ?>
         <div class='header'>
-            test
+            <?php echo view('header.php'); ?>
+        </div>
+        <div class='feed_body'>
+            <div class='write_feed'>
+                <form action="write_ok.php" method="post">
+                <div class="wi_line"></div>
+                <div id="in_content">
+                    <textarea name="content" id="ucontent" placeholder="What!?" required></textarea>
+                </div>
+                <div id="in_file">
+                    <input type="file" value="1" name="b_file" />
+                </div>
+                <div class="bt_se">
+                    <button type="submit">write</button>
+                </div>
+                </form>
+            </div>
         </div>
     <?php
     } else {
