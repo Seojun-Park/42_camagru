2567704f7c07071ba1e6ea3071ff61c9be596ce5
Jinchul park <jinchul112@gmail.com>
2020-10-22 12:09:39 +0200

    u

--- srcs/page/feed/reply_ok.php
+++ srcs/page/feed/reply_ok.php
@@ -2,15 +2,15 @@
 include $_SERVER['DOCUMENT_ROOT'] . "/db.php";
 
 
-    $bno = $_GET['idx'];
-    
-    if($bno && $_GET['userid'] && $_POST['content']){
-        $user = mq("select username from member where id='" .$_GET['userid']. "'");
-        $username = $user->fetch_array();
-        $sql = mq("insert into reply(feed_num,name,content) values('".$bno."','".$username['username']."','".$_POST['content']."')");
-        echo "<script>alert('댓글이 작성되었습니다.'); 
+$bno = $_GET['idx'];
+
+if ($bno && $_GET['userid'] && $_POST['content']) {
+    $user = mq("select username from member where id='" . $_GET['userid'] . "'");
+    $username = $user->fetch_array();
+    $sql = mq("insert into reply(feed_num,name,content) values('" . $bno . "','" . $username['username'] . "','" . $_POST['content'] . "')");
+    echo "<script>alert('댓글이 작성되었습니다.');
         location.href='../main.php';</script>";
-    }else{
-        echo "<script>alert('댓글 작성에 실패했습니다.'); 
+} else {
+    echo "<script>alert('댓글 작성에 실패했습니다.');
         history.back();</script>";
-    }
+}
