5873ac8ddcb8a6fee64f7a485782ff2f36b5c8b2
Jinchul park <jinchul112@gmail.com>
2020-10-22 12:25:40 +0200

    u

--- srcs/page/header.php
+++ srcs/page/header.php
@@ -1,55 +1,53 @@
 <!-- @format -->
-
 <!DOCTYPE html>
 
 <html>
-  <head>
-    <meta charset="utf-8" />
-    <link
-      rel="stylesheet"
-      href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css"
-    />
-    <link rel="stylesheet" href="../css/header.css" />
-  </head>
 
-  <body>
-    <header>
-      <div class="header_row first_col">
-        <a href="/">logo</a>
-      </div>
-      <div class="header_row second_col">
-        <form method="post" class="search_form">
-          <input type="text" id="searchbar" placeholder="Search user" />
-          <button type="submit" id="searchbutton" name="search">
-            <span class="glyphicon glyphicon-search"></span>
-          </button>
-        </form>
-      </div>
-      <div class="header_row third_col">
-        <div class="col_block">
-          <a href="/">
-            <button>profile</button>
+<head>
+  <meta charset="utf-8" />
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css" />
+  <link rel="stylesheet" href="../css/header.css" />
+</head>
+
+<body>
+  <header>
+    <div class="header_row first_col">
+      <a href="/page/main.php">logo</a>
+    </div>
+    <div class="header_row second_col">
+      <form method="post" class="search_form">
+        <input type="text" id="searchbar" placeholder="Search user" />
+        <button type="submit" id="searchbutton" name="search">
+          <span class="glyphicon glyphicon-search"></span>
+        </button>
+      </form>
+    </div>
+    <div class="header_row third_col">
+      <div class="col_block">
+        <a href="profile/profile.php?id=<?php echo $_SESSION['userid'] ?>">
+          <button>profile</button>
         </a>
-        </div>
-        <div class="col_block">
-          <a href="../member/logout.php">
-            <button>log out</button>
-          </a>
+      </div>
+      <div class="col_block">
+        <a href="../member/logout.php">
+          <button>log out</button>
+        </a>
+      </div>
+    </div>
+    <div class="header_row">
+      <div class="dropdown">
+        <button>
+          <span class="glyphicon glyphicon-align-justify"></span>
+        </button>
+        <div class="dropdown-content">
+          <form method="post">
+            <a href="profile/profile.php?id=<?php echo $_SESSION['userid'] ?>">profile</a>
+            <a href="../member/logout.php">logout</a>
+          </form>
         </div>
       </div>
-      <div class="header_row">
-        <div class="dropdown">
-          <button>
-            <span class="glyphicon glyphicon-align-justify"></span>
-          </button>
-          <div class="dropdown-content">
-            <form method="post">
-              <a href="/">profile</a>
-              <a href="../member/logout.php">logout</a>
-            </form>
-          </div>
-        </div>
-      </div>
-    </header>
-  </body>
+    </div>
+  </header>
+</body>
+
 </html>
---
+++ srcs/page/profile/profile.php
@@ -0,0 +1,23 @@
+<?php include $_SERVER['DOCUMENT_ROOT'] . "/hooks/func_view.php";
+?>
+
+<!DOCTYPE html>
+
+<html>
+
+<head>
+    <meta charset="utf-8" />
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <link rel="stylesheet" href="../../css/reset.css" />
+    <link rel="stylesheet" href="../../css/header.css" />
+
+    <title>Camagru</title>
+</head>
+
+<body>
+    <div class="header">
+        <?php echo view("../header.php") ?>
+    </div>
+</body>
+
+</html>
