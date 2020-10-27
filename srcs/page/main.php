aeac61084a57c3b65a2c71ea044deca397b62c18
Jinchul park <jinchul112@gmail.com>
2020-10-27 18:02:15 +0100

    u

--- srcs/page/main.php
+++ srcs/page/main.php
@@ -10,31 +10,26 @@ include $_SERVER['DOCUMENT_ROOT'] . "/hooks/func_view.php";
     <link rel="stylesheet" href="/css/main.css" />
     <title>Camagru</title>
     <script>
-        function stop(e) {
-            var stream = video.srcObject;
-            var tracks = stream.getTracks();
+        window.onload = async () => {
+            const video = document.getElementById('monitor');
+            const canvas = document.getElementById('photo');
+            const shutter = document.getElementById('shutter');
 
-            for (var i = 0; i < tracks.length; i++) {
-                var track = tracks[i];
-                track.stop();
-            }
-            video.srcObject = null;
-        }
+            try {
+                video.srcObject = await navigator.mediaDevices.getUserMedia({
+                    video: true
+                });
 
-        function start(e) {
-            var video = document.querySelector("#videoElement");
-            if (navigator.mediaDevices.getUserMedia) {
-                navigator.mediaDevices.getUserMedia({
-                        video: true
-                    })
-                    .then(function(stream) {
-                        video.srcObject = stream;
-                    })
-                    .catch(function(error) {
-                        console.log("Something went wrong!");
-                    });
+                await new Promise((resolve) => video.onloadedmetadata = resolve);
+                canvas.width = video.videoWidth;
+                canvas.height = video.videoHeight;
+                document.getElementById('app').hidden = false;
+
+                shutter.onclick = () => canvas.getContext('2d').drawImage(video, 0, 0);
+            } catch (err) {
+                console.error(err);
             }
-        }
+        };
     </script>
 </head>
 
@@ -47,27 +42,19 @@ include $_SERVER['DOCUMENT_ROOT'] . "/hooks/func_view.php";
         <div class='header'>
             <?php echo view('header.php'); ?>
         </div>
-        <div>
-            <div class="wrapper">
-                <div class="main_sec">
+        <div class="wrapper">
+            <div class="main_sec">
+                <section id="app" hidden>
                     <div class="camera_view">
-                        <video autoplay="true" id="videoElement"></video>
+                        <video id="monitor" autoplay></video>
+                        <button id="shutter">&#x1F4F7;</button>
                     </div>
-                    <div class="button_sec">
-                        <div class="camera_button">
-                            take
-                        </div>
-                        <div class="camera_button">
-                            <button onclick="start();">start</button>
-                        </div>
-                        <div class="camera_button">
-                            <button onclick="stop();">stop</button>
-                        </div>
-                    </div>
-                </div>
-                <div class="side_sec">
-                    second col
-                </div>
+                </section>
+            </div>
+            <div class="side_sec">
+                <section id="app" hidden>
+                    <canvas id="photo"></canvas>
+                </section>
             </div>
         </div>
         <div class="header">
