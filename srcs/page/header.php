cc1591dd9b04899fe9c944e408611af5625fc84e
Seojun_Park <jinchul112@gmail.com>
2020-10-28 11:39:39 +0100

    u

--- srcs/page/header.php
+++ srcs/page/header.php
@@ -1,48 +1,50 @@
 <!-- @format -->
+
 <!DOCTYPE html>
 
 <html>
 
 <head>
-  <meta charset="utf-8" />
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css" />
-  <link rel="stylesheet" href="/css/header.css" />
+    <meta charset="utf-8" />
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css" />
+    <link rel="stylesheet" href="../css/header.css" />
 </head>
 
 <body>
-  <header>
-    <div class="header_row first_col">
-      <a href="/page/main.php">logo</a>
-    </div>
-    <div class="header_row second_col">
-      empty for now
-    </div>
-    <div class="header_row third_col">
-      <div class="col_block">
-        <a href="profile/profile.php?id=<?php echo $_SESSION['userid'] ?>">
-          <button>profile</button>
-        </a>
-      </div>
-      <div class="col_block">
-        <a href="/member/logout.php">
-          <button>log out</button>
-        </a>
-      </div>
-    </div>
-    <div class="header_row">
-      <div class="dropdown">
-        <button>
-          <span class="glyphicon glyphicon-align-justify"></span>
-        </button>
-        <div class="dropdown-content">
-          <form method="post">
-            <a href="profile/profile.php?id=<?php echo $_SESSION['userid'] ?>">profile</a>
-            <a href="/member/logout.php">logout</a>
-          </form>
+    <header>
+        <div class="header_row first_col">
+            <a href="/">logo</a>
         </div>
-      </div>
-    </div>
-  </header>
+        <div class="header_row second_col">
+            empty for now
+        </div>
+        <div class="header_row third_col">
+            <div class="col_block">
+                <a href="/">
+                    <button>PROFILE</button>
+                </a>
+            </div>
+            <div class="col_block">
+                <a href="../member/logout.php">
+                    <button>LOGOUT</button>
+                </a>
+            </div>
+        </div>
+
+        <div class="header_row">
+            <div class="dropdown">
+                <button>
+            <span class="glyphicon glyphicon-align-justify"></span>
+          </button>
+                <div class="dropdown-content">
+                    <form method="post">
+                        <a href="/">PROFILE</a>
+                        <a href="../member/logout.php">LOGOUT</a>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </header>
 </body>
 
 </html>
