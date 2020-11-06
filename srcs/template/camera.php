23a9c9cab600bb1cb3fdff04d0092b900216cab7
Jinchul park <jinchul112@gmail.com>
2020-11-06 22:24:44 +0100

    u

--- srcs/template/camera.php
+++ srcs/template/camera.php
@@ -22,77 +22,6 @@ closedir($handle);
 <link rel="stylesheet" href="../css/camera.css" />
 <link rel="stylesheet" href="../css/header.css" />
 <script src="../js/html2canvas.js"></script>
-<script>
-    window.onload = async () => {
-        var video = document.getElementById('monitor');
-        const canvas = document.getElementById('pre_img');
-        const shutter = document.getElementsByClassName('shut');
-        const fix = document.getElementsByClassName('fix');
-        const retry = document.getElementsByClassName('re');
-
-        try {
-            video.srcObject = await navigator.mediaDevices.getUserMedia({
-                video: true
-            })
-            await new Promise((resolve) => video.onloadedmetadata = resolve);
-            canvas.width = video.videoWidth;
-            canvas.height = video.videoHeight;
-            document.getElementById("pre_img").hidden = true;
-
-        } catch (e) {
-            console.log(e);
-        }
-    }
-
-    const cameraShutter = () => {
-        var video = document.getElementById('monitor');
-        const canvas = document.getElementById('pre_img');
-        const stream = video.srcObject;
-        var tracks = stream.getTracks();
-
-        canvas.getContext("2d").drawImage(video, 0, 0);
-        html2canvas(document.querySelector("#pre_img")).then(canvas => {
-            document.querySelector("#preview").appendChild(canvas);
-        });
-        video.hidden = true;
-        document.getElementById("pre_img").hidden = false;
-        for (var i = 0; i < tracks.length; i++) {
-            var track = tracks[i];
-            track.stop();
-        }
-        video.srcObject = null;
-    }
-
-    const fix_image = () => {
-        const img = document.querySelector("#pre_img").getAttribute('src');
-        const canvas = document.getElementById('pre_img');
-        const input = document.getElementById('send_image');
-        var tmpImage = new Image();
-        var context = canvas.getContext("2d");
-        context.globalCompositeOperation = "source-over";
-        tmpImage.src = img;
-        tmpImage.onload = function() {
-            context.drawImage(this, 0, 0, 500, 500);
-            var dataURI = canvas.toDataURL("image/jpeg");
-            document.querySelector("#send_image").value = dataURI;;
-        }
-    }
-
-    const chosen_sticker = (index) => {
-        const img = document.getElementsByClassName('li_img')[index];
-        const canvas = document.getElementById("pre_img");
-        const uri = img.getAttribute('src');
-        var tmpImage = new Image();
-        tmpImage.src = uri;
-        var context = canvas.getContext("2d");
-        context.globalCompositeOperation = "source-over";
-        tmpImage.onload = function() {
-            context.drawImage(this, 280, 280, 250, 250);
-            var dataURI = canvas.toDataURL("image/jpeg");
-            document.querySelector("#photo").src = dataURI;
-        }
-    }
-</script>
 
 </head>
 
@@ -148,7 +77,7 @@ closedir($handle);
                 <div class="submit_btns">
                     <input type="button" id="submit_btn" class="shu" value="SHUTTER" onclick=cameraShutter(); />
                     <input type="button" id="submit_btn" class="fix" value="FIX" onclick=fix_image(); />
-                    <input type="submit" id="submit_btn" value="UPLOAD" name="submit" />
+                    <input type="submit" id="submit_btn" value="UPLOAD" name="submit" formaction="camera_ok.php" />
                     <input type="button" id="submit_btn" class="re" value="RETRY" />
                 </div>
             </form>
@@ -172,63 +101,77 @@ closedir($handle);
 <footer>
     Copyright &copy; Jin 2020 - All Rights Reserved
 </footer>
+<script>
+    window.onload = async () => {
+        var video = document.getElementById('monitor');
+        const canvas = document.getElementById('pre_img');
+        const shutter = document.getElementsByClassName('shut');
+        const fix = document.getElementsByClassName('fix');
+        const retry = document.getElementsByClassName('re');
+
+        try {
+            video.srcObject = await navigator.mediaDevices.getUserMedia({
+                video: true
+            })
+            await new Promise((resolve) => video.onloadedmetadata = resolve);
+            canvas.width = video.videoWidth;
+            canvas.height = video.videoHeight;
+            document.getElementById("pre_img").hidden = true;
+
+        } catch (e) {
+            console.log(e);
+        }
+    }
+
+    const cameraShutter = () => {
+        var video = document.getElementById('monitor');
+        const canvas = document.getElementById('pre_img');
+        const stream = video.srcObject;
+        var tracks = stream.getTracks();
+
+        canvas.getContext("2d").drawImage(video, 0, 0);
+        html2canvas(document.querySelector("#pre_img")).then(canvas => {
+            document.querySelector("#preview").appendChild(canvas);
+        });
+        video.hidden = true;
+        document.getElementById("pre_img").hidden = false;
+        for (var i = 0; i < tracks.length; i++) {
+            var track = tracks[i];
+            track.stop();
+        }
+        video.srcObject = null;
+    }
+
+    const fix_image = () => {
+        const img = document.querySelector("#pre_img").getAttribute('src');
+        const canvas = document.getElementById("pre_img");
+        var tmpImage = new Image();
+        var dataURI = canvas.toDataURL("image/jpeg");
+        var context = canvas.getContext("2d");
+        context.globalCompositeOperation = "source-over";
+        tmpImage.src = dataURI;
+        tmpImage.onload = function() {
+            // context.drawImage(this, 0, 0, 500, 500);
+            var dataURI = canvas.toDataURL("image/jpeg");
+            document.querySelector("#send_image").value = dataURI;;
+        }
+        console.log(document.getElementById("send_image"))
+    }
+
+    const chosen_sticker = (index) => {
+        const img = document.getElementsByClassName('li_img')[index];
+        const canvas = document.getElementById("pre_img");
+        const uri = img.getAttribute('src');
+        var tmpImage = new Image();
+        tmpImage.src = uri;
+        var context = canvas.getContext("2d");
+        context.globalCompositeOperation = "source-over";
+        tmpImage.onload = function() {
+            context.drawImage(this, 280, 280, 250, 250);
+            var dataURI = canvas.toDataURL("image/jpeg");
+            document.querySelector("#pre_img").src = dataURI;
+        }
+    }
+</script>
 
 </html>
-
-<!-- window.onload = async () => {
-            var video = document.getElementById('monitor');
-            const canvas = document.getElementById('photo');
-            const shutter = document.getElementById('shutter');
-            const stop = document.getElementById('stop');
-            const on = document.getElementById('on');
-            try {
-                video.srcObject = await navigator.mediaDevices.getUserMedia({
-                    video: true
-                });
-                await new Promise((resolve) => video.onloadedmetadata = resolve);
-                canvas.width = video.videoWidth;
-                canvas.height = video.videoHeight;
-                document.getElementById('app').hidden = false;
-                document.getElementsByClassName('preview').hidden = true;
-                document.getElementById('photo').hidden = true;
-
-                shutter.onclick = () => {
-                    canvas.getContext("2d").drawImage(video, 0, 0);
-                    html2canvas(document.querySelector("#photo")).then(canvas => {
-                        document.querySelector(".preview").appendChild(canvas);
-                    });
-                    video.hidden = true;
-                    document.getElementById('photo').hidden = false;
-                };
-
-                on.onclick = () => {
-                    if (navigator.mediaDevices.getUserMedia) {
-                        navigator.mediaDevices.getUserMedia({
-                                video: true
-                            })
-                            .then(function(stream) {
-                                video.srcObject = stream;
-                            })
-                    }
-                }
-                stop.onclick = () => {
-                    const stream = video.srcObject;
-                    var tracks = stream.getTracks();
-                    for (var i = 0; i < tracks.length; i++) {
-                        var track = tracks[i];
-                        track.stop();
-                    }
-                    video.srcObject = null;
-                    navigator.mediaDevices.getUserMedia({
-                            video: true
-                        })
-                        .then(function(stream) {
-                            video.srcObject = stream;
-                        })
-                    console.log("captured")
-                }
-            } catch (err) {
-                console.error(err);
-            }
-        };
-     -->
---
+++ srcs/template/camera_ok.php
@@ -0,0 +1,31 @@
+<?php
+
+function base64ToImage($base64_string, $output_file, $userid){
+    chdir("../../upload/$userid");
+    $file = fopen($output_file, "wb");
+    $data = explode(',', $base64_string);
+    fwrite($file, base64_decode($data[1]));
+    fclose($file);
+
+    return $output_file;
+}
+
+// echo var_dump($_POST['send']);
+
+$user = array();
+
+$user['username'] = "jinpark";
+$userid = $user['username'];
+
+$date = date('Y-m-d');
+$i = 0;
+
+$imagename = $userid . "_" . $i . ".jpg";
+
+if (isset($_POST['send'])) {
+    if (file_exists("../upload/$userid") === false) {
+      mkdir("../upload/$userid", 0777, true);
+    }
+    base64ToImage($_POST['send'], $imagename, $userid);
+    echo '<pre>';
+  }
